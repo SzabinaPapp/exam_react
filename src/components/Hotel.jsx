bde38746ed41f331f45f7d653d243280b141ca34
Szabina Papp <szilberharszabi@gmail.com>
2021-03-22 16:07:38 +0100

    fixed_order

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -8,7 +8,7 @@ const Hotel = ({ name, stars, city}) => {
   return (
     <div className="hotelContainer">
       <p>{name}</p>
-      {shown && <p>{stars} {city} </p>}
+      {shown && <p> {city} {stars} </p>}
       {shown ? <button onClick={() => setShown(false)}>Show less</button> : <button onClick={() => setShown(true)}>Show more</button>}
     </div>
   )
