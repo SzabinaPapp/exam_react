864abbed5cb34b1f748fcbdc82e3022217fe118d
Szabina Papp <szilberharszabi@gmail.com>
2021-03-22 16:04:48 +0100

    fixed_css

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,11 +1,12 @@
 import React, { useState} from "react";
+import "./Hotel.css"
 
 const Hotel = ({ name, stars, city}) => {
   
   const [shown, setShown] = useState(false);
   
   return (
-    <div>
+    <div className="hotelContainer">
       <p>{name}</p>
       {shown && <p>{stars} {city} </p>}
       {shown ? <button onClick={() => setShown(false)}>Show less</button> : <button onClick={() => setShown(true)}>Show more</button>}
